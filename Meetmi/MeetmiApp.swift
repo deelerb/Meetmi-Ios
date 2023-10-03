62f4cf7a823881f4a50e3bb0859039b50597b50d
deelerb <deelerb@gmail.com>
2023-10-03 11:49:23 +0200

    Initial Commit

---
+++ Meetmi/MeetmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetmiApp.swift
+//  Meetmi
+//
+//  Created by Dale Banda on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct MeetmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
